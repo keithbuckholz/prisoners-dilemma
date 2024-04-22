07f839ad2dc3353c6bbb102b1191bdaf283bb9be
Keith Buckholz <keithbuckholz@Keiths-Air-2.stdusr.yale.internal>
2024-04-22 12:30:42 -0400

    Credits and Populations basically functional. Fields not displaying in CL.

--- prisoners_dilemma/game/game.py
+++ prisoners_dilemma/game/game.py
@@ -178,39 +178,43 @@ class population_mode(dilemma_tournament):
 	the simulation ends.
 	"""
 
-	def __init__(self, players=None, n_rounds=None, field_size=(20, 20)):
+	def __init__(self, players=None, n_rounds=None, evolutions=None, field_size=(20, 20)):
 		super().__init__(players, n_rounds)
 		self.field = np.zeros(field_size)
 		enumeration = enumerate(define_players(players))
 		self.players = {number: player for number, player in enumeration}
-		# Temporary n_rounds value
-		self.n_rounds = 10
+		# Temporary evolutions value
+		self.evolutions = 10
+		self.score_array = np.zeros(field_size)
 
-	def spawn(self, replace_indicies=None):
-		if not replaces_indicies:
-			for ii in range(self.field.shape[0]):
-				for nn in range(self.field.shape[1]):
-					self.field[ii,nn] = self.rng.choice(list(self.players.keys()))
+		cube_shape = field_size + (2,)
+		self.field_cube = np.empty(cube_shape)
+		self.score_cube = np.empty(cube_shape)
+
+	def spawn(self):
+		for ii in range(self.field.shape[0]):
+			for nn in range(self.field.shape[1]):
+				self.field[ii,nn] = self.rng.choice(list(self.players.keys()))
 		return self
 
 	def round(self):
 		"""
 		Runs a single round 
 		"""
-		for ii in self.field.shape()[0]:
-			for nn in self.field.shape()[1]:
+		for ii in range(self.field.shape[0]):
+			for nn in range(self.field.shape[1]):
 
 				# Define this bot and identify neighbors
 				this_bot = self.field[ii, nn]
 				neighbor_indices = [
-					(ii - 1, nn),  # Top neighbor
+					# (ii - 1, nn),  # Top neighbor
 					(ii + 1, nn),  # Bottom neighbor
-					(ii, nn - 1),  # Left neighbor
+					# (ii, nn - 1),  # Left neighbor
 					(ii, nn + 1),  # Right neighbor
-					(ii - 1, nn - 1), # Top-left neighbor
+					# (ii - 1, nn - 1), # Top-left neighbor
 					(ii + 1, nn - 1), # Bottom-left neighbor
 					(ii + 1, nn + 1), # Bottom-right neighbor
-					(ii - 1, nn + 1) # Top-right neighbor
+					# (ii - 1, nn + 1) # Top-right neighbor
 				]
 
 				# Filter out edge cases
@@ -218,15 +222,79 @@ class population_mode(dilemma_tournament):
 										  if 0 <= jj < self.field.shape[0] and 
 										  0 <= kk < self.field.shape[1]]
 
-				# Turn neighbor indicies into functions
-				neighbors = [self.field[inidex] for index in valid_neighbor_indices]
+				# Gather list of neighbor functions with indicies
+				neighbors = [(self.field[index], index) for index in valid_neighbor_indices]
 				for neighbor in neighbors:
-					self.matchup(self.players[this_bot], self.players[neighbor])
-		self.n_rounds -= 1 
+					self.matchup(self.players[this_bot], 
+					             self.players[neighbor[0]], 
+								 (ii, nn), 
+								 neighbor[1])
+		
+		return self
+
+	def matchup(self, bot_1, bot_2, bot_1_loc, bot_2_loc):
+		"""
+		This method runs n rounds of the prisoners dilemma by calling the award_points method n times.
+		This method records all match information to all_results instance attribute, and awards points
+		to the final_score instance attribute.
+
+		Parameters
+		----------
+		bot_1: function
+			Player algorithm.
+		bot_2: function
+			Player algorithm.
+		"""
+		nn = self.n_rounds
+		if not nn: # Random number of rounds if not user defined
+			nn = round(self.rng.normal(200, 10)) # Normal ditribution mean=200, one_sigma=10
+		bot_1_history = []
+		bot_2_history = []
+
+		# Run game nn number of times
+		for ii in range(nn):
+
+			# Collect bot decisions
+			decision_1 = bot_1(bot_2_history)
+			decision_2 = bot_2(bot_1_history)
+
+			# Run dilemma, store score_tuple
+			score_tuple = self.award_points(decision_1, decision_2)
+
+			# Unpack score_tuple and update scores
+			self.score_array[bot_1_loc] += score_tuple[0]
+			self.score_array[bot_2_loc] += score_tuple[1]
+
+			# Format each rounds results
+			round_info = [[bot_1.__name__, decision_1, score_tuple[0]],
+						[bot_2.__name__, decision_2, score_tuple[1]]]
+
+			# Update this matches history
+			bot_1_history.append(round_info[0])
+			bot_2_history.append(round_info[1])
+
 		return self
 	
-	def lowest_value(self):
-		print("TODO")
+	def respawn(self):
+		# Store current field state
+		np.concatenate((self.field_cube, 
+						np.expand_dims(self.field, axis=2)), 
+						axis=2) # Add array to cube
+
+		# Determine indicies to respawn
+		replace_n = int((self.field.shape[0] * self.field.shape[1])/5)
+		flat_indices = np.argsort(self.field.flatten())
+		true_indicies = np.unravel_index(flat_indices[:replace_n], self.field.shape)
+
+		# Respawn those indicies
+		for ii in true_indicies[0]:
+			for nn in true_indicies[1]:
+				self.field[ii,nn] = self.rng.choice(list(self.players.keys()))
+
+		self.score_cube = np.concatenate((self.score_cube, 
+										  np.expand_dims(self.score_array, axis=2)), 
+										  axis=2) # Add array to cube
+		self.score_array = np.zeros(self.field.shape) # Empty array
 		return self
 
 	def display(self):
@@ -235,7 +303,6 @@ class population_mode(dilemma_tournament):
 	
 		No Parameters.
 		"""
-		self.spawn(init=True)
 		plt.imshow(self.field)
 		return self
 
@@ -250,10 +317,11 @@ class population_mode(dilemma_tournament):
 		"""
 		self.spawn()
 		self.display()
-		while self.n_rounds > 0:
+		while self.evolutions > 0:
 			self.round()
-			self.lowest_value()
-			self.spanw()
+			self.respawn()
+			self.evolutions -= 1
+			print(self.evolutions)
 		self.display()
 		return self
 
@@ -285,8 +353,7 @@ def population():
 	"""
 	Runs population simulation with built-in bots using command line
 	"""
-
-	population_mode().display()
+	population_mode().run().display()
 	return 0
 
 def credits():
