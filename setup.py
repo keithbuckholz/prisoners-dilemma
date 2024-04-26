c21def53fd25392f37dc5be9990fdbfa08d772a9
Keith Buckholz <keithbuckholz@Keiths-MacBook-Air-2.local>
2024-04-26 13:44:22 -0400

    fixing PYPI commpatibility

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setuptools.setup(
      author_email="keith.buckholz@yale.edu",
      description="Prisoner's Dilemma Simulation",
      long_description_content_type="text/markdown",
-     long_description=README,
+     long_description="README.md",
      packages=["prisoners_dilemma", "prisoners_dilemma/bots", "prisoners_dilemma/tournament", "prisoners_dilemma/population"],
      python_requires=">=3",
      install_requires=["numpy", "matplotlib", "imageio"],
